f1960d6bca66606e2d2a383a47f6d994b0fd9b2e
Sergey Misuk <sergey.misuk@valor-software.com>
2025-05-20 19:54:11 +0300

    Add value_mapper argument to open_csv, open_xls and open_xls_multi

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,8 +1,13 @@
+from collections.abc import Callable
 from pathlib import Path
+from typing import Any, TYPE_CHECKING
 
 import pytest
 
 from hope_smart_import.readers import open_csv, open_xls, open_xls_multi
+
+if TYPE_CHECKING:
+    from hope_smart_import.types import ValueMapper
 
 
 @pytest.fixture()
@@ -27,6 +32,12 @@ def test_read_csv_no_headers(csv: str, start_at: int) -> None:
         assert list(e.keys()) == ["column1", "column2", "column3"]
 
 
+@pytest.mark.parametrize(["mapper", "validator"], [(str.upper, str.isupper), (str.lower, str.islower)])
+def test_read_csv_value_mapper(csv: str, mapper: "ValueMapper", validator: Callable[[Any], bool]) -> None:
+    for row in open_csv(csv, headers=True, value_mapper=mapper):
+        assert all(map(validator, row.values()))
+
+
 @pytest.mark.parametrize("start_at", [0, 1])
 def test_read_xls_headers(xls: str, start_at: int) -> None:
     for e in open_xls(xls, start_at=start_at):
@@ -37,6 +48,12 @@ def test_read_xls_headers(xls: str, start_at: int) -> None:
 def test_read_xls_no_headers(xls: str, start_at: int) -> None:
     for e in open_xls(xls, start_at=start_at, headers=False):
         assert list(e.keys()) == ["column1", "column2", "column3"]
+
+
+@pytest.mark.parametrize(["mapper", "validator"], [(str.upper, str.isupper), (str.lower, str.islower)])
+def test_read_xls_value_mapper(xls: str, mapper: "ValueMapper", validator: Callable[[Any], bool]) -> None:
+    for row in open_xls(xls, headers=True, value_mapper=mapper):
+        assert all(map(validator, row.values()))
 
 
 @pytest.mark.parametrize("start_at", [0, 1])
@@ -61,3 +78,10 @@ def test_read_multi_xls_headers(xls: str, start_at: int) -> None:
         # assert ret[0] == (1, {"name": "John", "last_name": "Doe", "gender": "M"})
         # Sheet 1 - Row 1
         assert ret[1] == (1, {"name": "Jane1", "last_name": "Doe1", "gender": "f"})
+
+
+@pytest.mark.parametrize(["mapper", "validator"], [(str.upper, str.isupper), (str.lower, str.islower)])
+def test_read_multi_xls_value_mapper(xls: str, mapper: "ValueMapper", validator: Callable[[Any], bool]) -> None:
+    for _, sheet in open_xls_multi(xls, sheets=[0, 1], headers=True, value_mapper=mapper):
+        for row in sheet:
+            assert all(map(validator, row.values()))
